c9eedc45b1e0e5b3778f5d625ea62829e02d35dd
Vincentius Dwi Prasetyo <vincentius85@gmail.com>
2026-02-04 13:32:40 +0700

    feat: adding router for the navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import FormPage from "./Pages/FormPage";
+import ResultPage from "./Pages/ResultPage";
+import Navbar from "./components/Navbar";
+
 import React from 'react'
 
 export default function App() {
   return (
-    <div className='text-center'>App</div>
+    <BrowserRouter>
+      <Navbar/>
+      <Routes>
+        <Route path="/" element={<FormPage/>}/>
+        <Route path="/results" element={<ResultPage/>}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
